77b2191661178192918d11af4ddd1225a7688470
cyq7on <cyq7on@qq.com>
2018-08-19 22:03:55 +0800

    Two different dies

--- practice/die_visual.py
+++ practice/die_visual.py
@@ -2,13 +2,14 @@ import pygal
 
 from practice.die import Die
 
-die = Die()
+die1 = Die()
+die2 = Die(8)
 result = []
 for i in range(1000):
-    result.append(die.roll())
+    result.append(die1.roll() + die2.roll())
 
 freq = []
-r = range(1, die.num_sides + 1)
+r = range(2, die1.num_sides + die2.num_sides + 1)
 for roll in r:
     freq.append(result.count(roll))
 print(freq)
@@ -17,5 +18,6 @@ bar = pygal.Bar()
 # bar.x_labels = [str(x) for x in r]
 bar.x_labels = map(str, r)
 print(bar.x_labels)
-bar.add("D6", freq)
+bar.add("D6 + D6", freq)
 bar.render_in_browser()
+
